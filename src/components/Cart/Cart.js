1159669e7a14a5662309abf92ac78ca76e17aa57
EugeneBerd <75495906+EugeneBerd@users.noreply.github.com>
2021-09-03 14:11:18 +0300

    Delete src directory

--- src/components/Cart/Cart.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-class Cart extends PureComponent {
-  render() {
-    return <div>Товаров в Корзине: {this.props.cart}</div>;
-  }
-}
-
-export default Cart;
